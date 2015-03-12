7a9d31ac57777148c6d2aba26bf1e53af28a3073
V0906 <vasenka.terkin.72@inbox.ru>
2015-03-12 12:16:23 -0400

    added Page

---
+++ src/test/java/com/elena/pages/PageTest.java
@@ -0,0 +1,15 @@
+package com.elena.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class PageTest extends BaseTest {
+
+	public PageTest(WebDriver driver) {
+		super(driver);
+	}	
+		public void printTest(){
+			System.out.println("Hello World");
+		}
+	
+
+}
